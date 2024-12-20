e057516eb1ee0dcc7501329d0162b6ee7cfb22fa
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2024-12-20 14:57:31 +0530

    we test all address api and modify it where we get error we fixed it.

--- server/src/controllers/address/updateAddress.controller.js
+++ server/src/controllers/address/updateAddress.controller.js
@@ -1,11 +1,13 @@
 import { asyncHandler } from "../../errors/asynHandler.js";
+import ErrorHandler from "../../errors/errorHandler.js";
 import { shippingAddress } from "../../models/shippingAddress.model.js";
 
 export const updateAddress = asyncHandler(async (req , res , next) => {
     const addr = req.body 
     const {addrId} = req.params 
-
-    await shippingAddress.findByIdAndUpdate(addrId , addr , {runValidators : true , new : true})
+    const address = await shippingAddress.findById(addrId)
+    if(!address) return next(new ErrorHandler("Address not found !",404))
+    await shippingAddress.findByIdAndUpdate(addrId , addr , {runValidators : true , new : true , useFindAndModify : false})
 
     res.status(200).json({
         success : true,
