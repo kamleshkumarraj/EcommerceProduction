31f17e696b087f5648769aff33f82e6511f20672
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2024-12-29 09:36:56 +0530

    we create store for and reducers for blog handling

---
+++ client/src/store/slices/blog.slice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const blogSlice = createSlice({
+    name : "blog",
+    initialState : {
+        totalBlog : [],
+        mySavedBlogs : [],
+        myLikedBlogs : [],
+        myCreatedBlogs : []
+    },
+    reducers : {
+        setAllBlogs : (state , action) => {
+            state.totalBlog = action.payload;
+        },
+        setMySavedBlogs : (state , action) => {
+            state.mySavedBlogs = action.payload;
+        },
+        setMyLikedBlogs : (state , action) => {
+            state.myLikedBlogs = action.payload;
+        },
+        setMyCreatedBlogs : (state , action) => {
+            state.myCreatedBlogs = action.payload;
+        }
+    }
+})
+
+export const blogReducers = blogSlice.reducer;
+export const {setAllBlogs , setMyCreatedBlogs , setMyLikedBlogs , setMySavedBlogs} = blogSlice.actions;
+export const getTotalBlogs = (state) => state.blogs.totalBlog;
+export const getMySavedBlogs = (state) => state.blogs.mySavedBlogs;
+export const getMyLikedBlogs = (state) => state.blogs.myLikedBlogs;
+export const getMyCreatedBlogs = (state) => state.blogs.myCreatedBlogs;
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,24 +1,24 @@
-import {configureStore} from '@reduxjs/toolkit'
-import {selfHandler} from './slices/selfHandler.slice.js'
-import {apiHandlingReducers} from './slices/apiResonseHandler.slice.js'
-import {productsHandler} from './slices/productsHandler.slice.js'
-import {cartReducer} from './slices/cart.slice.js'
-import { wishlistReducer } from './slices/wishlist.slice.js'
-import { addressHandlerReducer } from './slices/addressHandler.slice.js'
-import { orderedProductsReducer } from './slices/orderItems.js'
-import { orderHandlerReducer } from './slices/order.slice.js'
-
+import { configureStore } from "@reduxjs/toolkit";
+import { selfHandler } from "./slices/selfHandler.slice.js";
+import { apiHandlingReducers } from "./slices/apiResonseHandler.slice.js";
+import { productsHandler } from "./slices/productsHandler.slice.js";
+import { cartReducer } from "./slices/cart.slice.js";
+import { wishlistReducer } from "./slices/wishlist.slice.js";
+import { addressHandlerReducer } from "./slices/addressHandler.slice.js";
+import { orderedProductsReducer } from "./slices/orderItems.js";
+import { orderHandlerReducer } from "./slices/order.slice.js";
+import { blogReducers } from "./slices/blog.slice.js";
 
 export const store = configureStore({
-    reducer : {
-        self : selfHandler,
-        apiResponse : apiHandlingReducers,
-        productsList : productsHandler,
-        ordersList : orderHandlerReducer,
-        cart : cartReducer,
-        wishlist : wishlistReducer,
-        address : addressHandlerReducer,
-        orderedProductsList : orderedProductsReducer,
-
-    }
-})
+  reducer: {
+    self: selfHandler,
+    apiResponse: apiHandlingReducers,
+    productsList: productsHandler,
+    ordersList: orderHandlerReducer,
+    cart: cartReducer,
+    wishlist: wishlistReducer,
+    address: addressHandlerReducer,
+    orderedProductsList: orderedProductsReducer,
+    blogs: blogReducers,
+  },
+});
