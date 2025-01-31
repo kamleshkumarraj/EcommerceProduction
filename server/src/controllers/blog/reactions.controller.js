ede97599ebbff4428e40d2016d89a47547bc0bd7
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-01-31 15:15:40 +0530

    we create the reaction for blog and comments also

--- server/src/controllers/blog/reactions.controller.js
+++ server/src/controllers/blog/reactions.controller.js
@@ -3,6 +3,8 @@ import { asyncHandler } from "../../errors/asynHandler.js";
 import { Comments } from "../../models/blog/comments.models.js";
 import ErrorHandler from "../../errors/errorHandler.js";
 import { ReplyComments } from "../../models/blog/replyComment.model.js";
+import { BlogReactions } from "../../models/blog/blogReactions.model.js";
+import { CommentReactions } from "../../models/blog/commentReactions.model.js";
 
 export const createComment = asyncHandler(async (req, res, next) => {
     const {blogId  , comment } = req.body;
@@ -31,3 +33,42 @@ export const createReplyForComment = asyncHandler(async (req, res, next) => {
         message : "You create reply successfully for comment.",
     })
 })
+
+export const createReactionForBlog = asyncHandler(async (req, res, next) => {
+    const {blogId , reactionType} = req.body;
+
+    if(mongoose.isValidObjectId(blogId) == false){
+        return next(new ErrorHandler("Please send valid blog id !",400));
+    }
+
+    const existReaction = await BlogReactions.findOne({blogId , creator : req.user.id});
+
+    if(existReaction) {
+        BlogReactions.updateOne({blogId , creator : req.user.id}, {$set : {reaction : reactionType}})
+    }else {
+        await BlogReactions.create({blogId , creator : req.user.id , reaction : reactionType})
+    }
+
+    res.status(200).json({
+        success : true,
+        message : "Reaction created successfully for blog.",
+    })
+})
+
+export const createReactionForComments = asyncHandler(async (req, res, next) => {
+    const {commentId , reactionType} = req.body;
+
+    if(mongoose.isValidObjectId(commentId) == false){
+        return next(new ErrorHandler("Please send valid comment id !",400));
+    }
+
+    const existReaction = await CommentReactions.findOne({commentId , creator : req.user.id});
+    if(existReaction) {
+        await CommentReactions.updateOne({commentId , creator : req.user.id}, {$set : {reaction : reactionType}})
+    }else{
+        await CommentReactions.create({commentId, creator : req.user.id, reaction : reactionType})
+    }
+})
+
+
+
