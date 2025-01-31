85edcb8ce006a3d3e8ba218abe39eecc6090bf84
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-01-31 06:28:00 +0530

    we fix the getMyCreatedBlog

--- server/src/controllers/blog/productsBlog.controller.js
+++ server/src/controllers/blog/productsBlog.controller.js
@@ -210,47 +210,9 @@ export const getAllProductsBlog = asyncHandler(async (req, res, next) => {
 });
 
 export const getMyCreatedProductBlogs = asyncHandler(async (req, res, next) => {
-  const myCreatedProductBlogs = await ProductsBlogs.aggregate([
-    {
-      $match: { creator: req.user.id },
-    },
-    {
-      $lookup: {
-        from: 'User',
-        localField: 'creator',
-        foreignField: '_id',
-        as: creatorDetails,
-        pipeline: [
-          {
-            $project: {
-              creatorName: { $concat: ['$firstName', ' ', '$lastName'] },
-              avatar: 1,
-              email: 1,
-              username: 1,
-            },
-          },
-        ],
-      },
-    },
-    {
-      $project: {
-        creatorDetails: 1,
-        title: 1,
-        content: 1,
-        summary: 1,
-        slug: 1,
-        category: 1,
-        thumbnail: 1,
-        images: 1,
-        subCategory: 1,
-        createdAt: 1,
-        updatedAt: 1,
-      },
-    },
-    { $sort: { createdAt: -1 } },
-    { $skip: skip },
-    { $limit: limit },
-  ]);
+  const myCreatedProductBlogs = await ProductsBlogs.aggregate(
+    blogFindQuery({matchQuery : {creator : req?.user?._id} , skip, limit})
+  );
 
   return res.status(200).json({
     success: true,
