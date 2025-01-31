1ae5707e95dd4185032359f8290cfd3f3cdc0f40
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-01-31 06:07:51 +0530

    we remove all unnecessary routes for blog related

--- server/src/routes/blog/blog.routes.js
+++ server/src/routes/blog/blog.routes.js
@@ -1,15 +1,10 @@
 import { Router } from 'express';
 import {
   createBlog,
-  createComment,
-  createReactions,
   deleteBlog,
-  getAllActionBlog,
   getAllBlogs,
   getCategoryBlog,
-  getCommentsForABlog,
-  getMyBlogs,
-  replyBlogComment,
+  getMyBlogs
 } from '../../controllers/blog/blog.controller.js';
 import { uploads } from '../../middlewares/fileUploads/userPhotoUploads.js';
 import isLoggedIn from '../../middlewares/isLoggedIn.middleware.js';
@@ -34,17 +29,7 @@ blogRouter.route('/create').post(
   createBlog,
 );
 
-blogRouter.route('/create/comment/:blogId').post(createComment);
-
 blogRouter.route('/my-blog').get(getMyBlogs);
-
-blogRouter.route('/get-all-comments/:blogId').get(getCommentsForABlog);
-
-blogRouter.route('/reply/:commentId').patch(replyBlogComment);
-
-blogRouter.route('/create-reactions/:blogId').patch(createReactions);
-
-blogRouter.route('/get-all-reactions/:blogId').get(getAllActionBlog);
 
 blogRouter.route('/delete/:blogId').delete(deleteBlog);
 
