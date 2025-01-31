cb6c9f4d81d85ecf4bd31e9ab25287458d063416
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-01-31 06:26:52 +0530

    we write the appropiate controller for finding all blogs

--- server/src/controllers/blog/productsBlog.controller.js
+++ server/src/controllers/blog/productsBlog.controller.js
@@ -5,7 +5,98 @@ import {
   removeMultipleFileFromCloudinary,
   uploadMultipleFilesOnCloudinary,
 } from '../../helper/helper.js';
+import { comments } from '../../models/blog/comments.models.js';
 import { ProductsBlogs } from '../../models/blog/productsBlog.model.js';
+
+const blogFindQuery =  ({matchQuery , limit, skip,}) => [
+    {
+      $match: matchQuery,
+    },
+    {
+      $lookup: {
+        from: 'User',
+        localField: 'creator',
+        foreignField: '_id',
+        as: creatorDetails,
+        pipeline: [
+          {
+            $project: {
+              creatorName: { $concat: ['$firstName', ' ', '$lastName'] },
+              avatar: 1,
+              email: 1,
+              username: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: 'comments',
+        localField: '_id',
+        foreignField: 'blogId',
+        as: comments,
+        pipeline: [
+          {
+            $count: 'commentsCounts',
+          },
+          {
+            $project: {
+              _id: 0,
+              commentsCounts: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: 'blogReactions',
+        localField: '_id',
+        foreignField: 'blogId',
+        as: blogReactions,
+        pipeline: [
+          {
+            $group: {
+              _id: '$reaction',
+              count: { $sum: 1 },
+            },
+          },
+          {
+            $project: {
+              reactionType: '$_id',
+              count: 1,
+            },
+          },
+        ],
+      },
+    },
+
+    {
+      $unwind: '$creatorDetails',
+    },
+
+    {
+      $project: {
+        creatorDetails: 1,
+        title: 1,
+        content: 1,
+        summary: 1,
+        slug: 1,
+        category: 1,
+        thumbnail: 1,
+        images: 1,
+        subCategory: 1,
+        createdAt: 1,
+        updatedAt: 1,
+        comments: 1,
+        blogReactions : 1
+      },
+    },
+    { $sort: { createdAt: -1 } },
+    { $skip: skip },
+    { $limit: limit },
+  ]
 
 export const createBlog = asyncHandler(async (req, res, next) => {
   const thumbnailData = req?.files?.thumbnail || [];
@@ -107,9 +198,21 @@ export const getAllBlogsDetailsCategoriesWise = asyncHandler(
 export const getAllProductsBlog = asyncHandler(async (req, res, next) => {
   const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
-  const productsBlogs = await ProductsBlogs.aggregate([
+  const productsBlogs = await ProductsBlogs.aggregate(
+    blogFindQuery({matchQuery : {} , skip, limit})
+);
+
+  return res.status(200).json({
+    success: true,
+    message: 'You get all products blogs successfully.',
+    data: productsBlogs,
+  });
+});
+
+export const getMyCreatedProductBlogs = asyncHandler(async (req, res, next) => {
+  const myCreatedProductBlogs = await ProductsBlogs.aggregate([
     {
-      $match: {},
+      $match: { creator: req.user.id },
     },
     {
       $lookup: {
@@ -150,75 +253,24 @@ export const getAllProductsBlog = asyncHandler(async (req, res, next) => {
   ]);
 
   return res.status(200).json({
-    success : true,
-    message : "You get all products blogs successfully.",
-    data : productsBlogs
-  })
+    success: true,
+    message: 'You get all your created products blogs successfully.',
+    data: myCreatedProductBlogs,
+  });
 });
 
-export const getMyCreatedProductBlogs = asyncHandler(async (req, res, next) => {
-    const myCreatedProductBlogs = await ProductsBlogs.aggregate([
-        {
-          $match: {creator : req.user.id},
-        },
-        {
-          $lookup: {
-            from: 'User',
-            localField: 'creator',
-            foreignField: '_id',
-            as: creatorDetails,
-            pipeline: [
-              {
-                $project: {
-                  creatorName: { $concat: ['$firstName', ' ', '$lastName'] },
-                  avatar: 1,
-                  email: 1,
-                  username: 1,
-                },
-              },
-            ],
-          },
-        },
-        {
-          $project: {
-            creatorDetails: 1,
-            title: 1,
-            content: 1,
-            summary: 1,
-            slug: 1,
-            category: 1,
-            thumbnail: 1,
-            images: 1,
-            subCategory: 1,
-            createdAt: 1,
-            updatedAt: 1,
-          },
-        },
-        { $sort: { createdAt: -1 } },
-        { $skip: skip },
-        { $limit: limit },
-      ]);
+export const deleteBlog = asyncHandler(async (req, res, next) => {
+  const { blogId } = req.params;
+  const deletedBlog = await ProductsBlogs.findByIdAndDelete(blogId);
 
-    return res.status(200).json({
-        success : true,
-        message : "You get all your created products blogs successfully.",
-        data : myCreatedProductBlogs
-      })
-    
-})
+  const thumbnail = deletedBlog.thumbnail;
+  const images = deletedBlog.images;
 
-export const deleteBlog = asyncHandler(async (req , res , next) => {
-    const {blogId} = req.params;
-    const deletedBlog = await ProductsBlogs.findByIdAndDelete(blogId);
+  const status = await removeMultipleFileFromCloudinary([thumbnail, ...images]);
+  if (status.success == false) return next(new ErrorHandler(status.error, 400));
 
-    const thumbnail = deletedBlog.thumbnail;
-    const images = deletedBlog.images;
-
-    const status = await removeMultipleFileFromCloudinary([thumbnail , ...images])
-    if(status.success == false) return next(new ErrorHandler(status.error, 400))
-
-    res.status(200).json({
-        success : true,
-        message : "Blog deleted successfully"
-    })
-})
+  res.status(200).json({
+    success: true,
+    message: 'Blog deleted successfully',
+  });
+});
