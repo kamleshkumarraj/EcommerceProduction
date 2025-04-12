305b8f0ba8fbdc4f26565b4eddc8e1f854533836
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-04-12 18:40:13 +0530

    we responsive the title of featureprductcard

--- client/src/components/body/FeaturedProductsBody.jsx
+++ client/src/components/body/FeaturedProductsBody.jsx
@@ -4,10 +4,10 @@ import PropTypes from "prop-types";
 const FeaturedProductsBody = ({ title, products }) => {
   return (
     <div id="featured-product-body" className=" py-[10px] ">
-      <div className="text-center flex justify-center items-center flex-col text-[3.6rem] mt-[20px] text-slate-600 font-bold relative pb-[45px]">
-        <h2>{title}</h2>
-        <div className="w-[200px] h-[4px] bg-[#7fad39] mt-4"></div>
-      </div>
+      <div className="text-center flex justify-center items-center flex-col 2xl:text-[3.6rem] xl:text-[3.2rem] md:text-[3rem] sm:text-[2.7rem] text-[2.4rem] text-slate-600 font-bold relative 2xl:pb-[45px] xl:pb-[40px] md:px-[35px] pb-[30px]">
+          <h2>{title}</h2>
+          <div className="2xl:w-[200px] xl:w-[170px] md:w-[150px] s:w-[130px] w-[100px] h-[4px] bg-[#7fad39] mt-4"></div>
+        </div>
       <div className="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-[20px]">
         {products &&
           products.length > 0 &&
--- client/src/components/body/LatestProductsBody.jsx
+++ client/src/components/body/LatestProductsBody.jsx
@@ -5,9 +5,9 @@ const LatestProductsBody = ({ title, products }) => {
   return (
     <div className="flex flex-wrap">
       <div className="w-full">
-        <div className="text-center flex justify-center items-center flex-col text-[3.6rem] text-slate-600 font-bold relative pb-[45px]">
+        <div className="text-center flex justify-center items-center flex-col 2xl:text-[3.6rem] xl:text-[3.2rem] md:text-[3rem] sm:text-[2.7rem] text-[2.4rem] text-slate-600 font-bold relative 2xl:pb-[45px] xl:pb-[40px] md:px-[35px] pb-[30px]">
           <h2>{title}</h2>
-          <div className="w-[200px] h-[4px] bg-[#7fad39] mt-4"></div>
+          <div className="2xl:w-[200px] xl:w-[170px] md:w-[150px] s:w-[130px] w-[100px] h-[4px] bg-[#7fad39] mt-4"></div>
         </div>
       </div>
       <div className="grid w-full gap-6 2xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1">
