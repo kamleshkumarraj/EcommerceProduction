3365942c790da730b793a476a11daa18a2a33f2a
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2026-03-04 14:17:24 +0530

    we add uploads in dir

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# 1D array
+arr1 = np.array([1, 2, 3, 4])
+print("1D Array:", arr1)
+
+# 2D array
+arr2 = np.array([[1, 2, 3],
+                 [4, 5, 6]])
+print("2D Array:\n", arr2)
+
+# Zeros and Ones
+zeros = np.zeros((2, 3))
+ones = np.ones((3, 2))
+
+print("Zeros:\n", zeros)
+print("Ones:\n", ones)
